dc55e19bf38adcdf8968f04217dc294bda98aaef
Taras Labiak <taras.labiak@stellarsolvers.com>
2020-03-08 13:23:21 +0200

    + prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { Subject } from 'rxjs';
+
+async function fetchJSON(url) {
+  const r = await fetch(url);
+  const json = await r.json();
+  console.log(json);
+  return json;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'labiak-ui';
+
+
+  constructor() {
+    fetchJSON('http://data.gov.ua/dataset/8851831d-b5ce-4ca8-8685-eafbc3f57eca/resource/6cfff17e-84ac-4141-b0fd-89abb68e9f31/download/mvswantedbezvesti_1.json'); 
+  }
+
 }
