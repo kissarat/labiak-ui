02c189798c41ed5f2931012e81f4ee36eafc9076
Taras Labiak <taras.labiak@stellarsolvers.com>
2020-04-05 02:15:32 +0300

    replace: express.js -> fastify

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const app = express();
+const fastify = require('fastify');
+const app = fastify({
+  logger: true
+});
 
 app.get('/', (req, res) => res.redirect('/api/v1'))
 app.get('/api/v1', function (req, res) {
-  res.send(`Hello World, ${req.connection.remoteAddress}`);
+  res.send(`Hello World, ${req.ip}`);
 });
 
 app.listen(+process.env.PORT || 3000);
