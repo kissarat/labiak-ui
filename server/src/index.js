8a298b4033b5a68a743dd5466cc23248a2fef4c5
Taras Labiak <taras.labiak@stellarsolvers.com>
2020-04-05 05:58:42 +0300

    + router

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,10 @@
 const fastify = require('fastify');
+const router = require('./routes');
 const app = fastify({
   logger: true
 });
 
-app.get('/', (req, res) => res.redirect('/api/v1'))
-app.get('/api/v1', function (req, res) {
-  res.send(`Hello World, ${req.ip}`);
-});
+app.use('/api/v1', router);
+app.get('/', (req, res) => res.redirect('/api/v1/healthcheck'))
 
 app.listen(+process.env.PORT || 3000);
---
+++ server/src/routes/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+const app = express();
+
+router.use((req, res, next) => {
+  Object.setPrototypeOf(req, app.request);
+  Object.setPrototypeOf(res, app.response);
+  req.res = res;
+  res.req = req;
+  next();
+});
+
+router.get('/healthcheck', (req, res) => {
+  res.json({
+    ok: 1,
+    ip: req.ip
+  });
+});
+
+module.exports = router;
