3c3b5c713ee789821961b9212aefc5ff958dbd67
Taras Labiak <taras.labiak@stellarsolvers.com>
2020-04-12 02:33:20 +0300

    + responseHeaders

--- server/src/db/schema.sql
+++ server/src/db/schema.sql
@@ -1,27 +1,24 @@
 create table "Origin" (
-  "originId" serial primary key,
+  "originId" bigserial primary key,
   "host" varchar(100) not null,
   "port" smallint not null default 443
 );
 
 create table "Url" (
-  "urlId" serial primary key,
+  "urlId" bigserial primary key,
   "originId" int not null references "Origin"("originId"),
   "path" varchar(200) not null,
   "params" json
 );
 
 create table "Request" (
-  "requestId" serial primary key,
+  "requestId" bigserial primary key,
   "urlId" int not null references "Url"("urlId"),
   "number" int not null,
-  "times" smallint not null default 1
-);
-
-create table "Response" (
-  "responseId" bigserial primary key,
-  "requestId" int not null references "Request"("requestId"),
-  "status" smallint not null
+  "speed" smallint not null default 1,
+  "after" timestamp
+  "status" smallint,
+  "responseHeaders" json
 );
 
 create view "UrlString" as
