b8ef1c3e6591c685fea8acb4751326bb376cf8bd
Taras Labiak <taras.labiak@stellarsolvers.com>
2020-04-18 17:34:15 +0300

    + src/services/response.js

--- server/src/services/response.js
+++ server/src/services/response.js
@@ -1,7 +1,13 @@
-class Success {
+class HttpResponse {
+  constructor() {
+    this.type = this.constructor.name;
+  }
+}
+
+class Success extends HttpResponse {
   constructor(data, count = data.length) {
+    super();
     this.ok = 1;
-    this.type = 'Success';
     this.count = count;
     this.data = data;
   }
